73beb16ab7c6b9d157bb2a88218731056cdacb96
david <d.cain2740@gmail.com>
2023-09-17 16:36:58 -0500

    adding opencv to main

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 
+#include <opencv2/opencv.hpp>
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -16,8 +17,24 @@ using std::endl;
 
 int MAIN(int argc, char** argv) {
   std::cout << "Hey there buddy!" << std::endl;
+  
+	if (argc != 2) {
+		printf("usage: DisplayImage.out <Image_Path>\n");
+		return -1;
+	}
+  cv::Mat image;
+  image = cv::imread(argv[1], 1);
+	if (!image.data) {
+		printf("No image data \n");
+		return -1;
+	}
+	cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Display Image", image);
+	cv::waitKey(0);
+  
   return EXIT_SUCCESS;
 }
+
 
 // just some function to have something that can be test
 #include <exception>
