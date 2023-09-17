f83fafc50d8af4516a6c5e4cff1196e92c18b1eb
david <d.cain2740@gmail.com>
2023-09-17 13:10:46 -0500

    new structure works

---
+++ main/src/main.cpp
@@ -0,0 +1,29 @@
+#include "main.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <fstream>
+#include <string>
+
+// when testing, the main of gtest and this main function will collide,
+// this prevents the collision, but it's an ugly hack like all ifdef branches
+#ifdef UNIT_TESTS
+#define MAIN not_main
+#else
+#define MAIN main
+#endif
+
+int MAIN(int argc, char** argv) {
+  std::cout << "Hey there buddy!" << std::endl;
+  return EXIT_SUCCESS;
+}
+
+
+// just some function to have something that can be test
+#include <exception>
+
+void GetFrobnicationInterval() {
+  throw std::string("InvalidOperation: frobnication interval can't be retrieved");
+}
+
