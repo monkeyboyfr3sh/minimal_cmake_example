a0e2e0f4bddca7a08d864c9c4d8346655e7f1e02
david <d.cain2740@gmail.com>
2023-09-17 12:56:33 -0500

    new structure coming

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,10 +5,6 @@ using std::cout;
 using std::endl;
 #include <fstream>
 #include <string>
-// there are other ways of getting a path relative to the executable, but this
-// one is part of the STL soon (if you can talk about soon in c++ development)
-#include <boost/filesystem.hpp>
-using boost::filesystem::absolute;
 
 // when testing, the main of gtest and this main function will collide,
 // this prevents the collision, but it's an ugly hack like all ifdef branches
@@ -19,15 +15,7 @@ using boost::filesystem::absolute;
 #endif
 
 int MAIN(int argc, char** argv) {
-  // this way you can have a path relative to the executable
-  // the absolute also gets rid of symlinks, so it should work pretty stable
-  auto resource_path = absolute(argv[0]).parent_path() / "resources";
-  std::fstream resource1((resource_path / "resource1").string());
-  std::string line;
-  while(resource1) {
-    std::getline(resource1, line);
-    cout << line << endl;
-  }
+  std::cout << "Hey there buddy!" << std::endl;
   return EXIT_SUCCESS;
 }
 
